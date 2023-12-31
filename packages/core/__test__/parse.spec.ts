59bb997b954f3e44e5ea67907f90abee6fa1c332
Ubugeeei <ubuge1122@gmail.com>
2023-12-31 22:08:01 +0900

    test: add test cases

--- packages/core/__test__/parse.spec.ts
+++ packages/core/__test__/parse.spec.ts
@@ -1,8 +1,8 @@
 import { describe, it, expect } from "vitest";
 import { parse } from "../src/parsing";
 
-describe("success to parse", () => {
-  it("basic single parsing", () => {
+describe("success to single var parse", () => {
+  it("basic parsing", () => {
     const result = parse("STAGE_NAME=development");
     expect(result.ok).toBe(true);
     if (result.ok) {
@@ -146,6 +146,85 @@ describe("success to parse", () => {
         ],
         span: { start: 0, end: 34 },
       });
+    }
+  });
+});
+
+describe("success to multi vars parse", () => {
+  it("should parse", () => {
+    const result = parse(`STAGE_NAME=development
+PORT=3000`);
+    expect(result.ok).toBe(true);
+    if (result.ok) {
+      expect(result.value).toMatchSnapshot();
+    }
+  });
+
+  it("should parse with space head", () => {
+    const result = parse(`
+    STAGE_NAME=development
+PORT=3000`);
+    expect(result.ok).toBe(true);
+    if (result.ok) {
+      expect(result.value).toMatchSnapshot();
+    }
+  });
+
+  it("should parse with double quat value", () => {
+    const result = parse(`
+    STAGE_NAME="development"
+PORT="3000"
+PORT2 = "3000"
+`);
+    expect(result.ok).toBe(true);
+    if (result.ok) {
+      expect(result.value).toMatchSnapshot();
+    }
+  });
+
+  it("should parse with double quat value with spaces", () => {
+    const result = parse(`
+    STAGE_NAME="
+    development
+"
+
+    PORT="3000"
+
+PORT2 = "
+3000"
+`);
+    expect(result.ok).toBe(true);
+    if (result.ok) {
+      expect(result.value).toMatchSnapshot();
+    }
+  });
+
+  it("should parse mixed double quat and no double quot values", () => {
+    {
+      const result = parse(`STAGE_NAME="development"
+  PORT=3000`);
+      expect(result.ok).toBe(true);
+      if (result.ok) {
+        expect(result.value).toMatchSnapshot();
+      }
+    }
+
+    {
+      const result = parse(`STAGE_NAME=development
+  PORT="3000"`);
+      expect(result.ok).toBe(true);
+      if (result.ok) {
+        expect(result.value).toMatchSnapshot();
+      }
+    }
+
+    {
+      const result = parse(`STAGE_NAME =     development
+  PORT    ="3000"`);
+      expect(result.ok).toBe(true);
+      if (result.ok) {
+        expect(result.value).toMatchSnapshot();
+      }
     }
   });
 });
